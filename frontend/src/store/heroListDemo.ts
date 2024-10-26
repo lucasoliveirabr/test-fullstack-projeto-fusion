045bf04b00c83c0a94c90ab260bffc7d0786d5d1
lucasoliveirabr <lucas.oliveirabr@outlook.com>
2024-10-26 19:01:14 +0100

    chore: update heroListDemo

--- frontend/src/store/heroListDemo.ts
+++ frontend/src/store/heroListDemo.ts
@@ -17,37 +17,37 @@ type HeroStore = {
 export const useHeroListDemoStore = create<HeroStore>()((set) => ({
   heroList: [
     {
-      id: 1633036800000,
+      id: 1,
       name: "Doutor Estranho",
       powersAndAbilities: "Magia, Artista Marcial, Manipulação de Energia, Teletransporte, Cronocinese.",
       origin: "Pensilvânia, Estados Unidos da América, Terra."
     },
     {
-      id: 1672531199000,
-      name: "Homem Aranha",
+      id: 2,
+      name: "Homem-Aranha",
       powersAndAbilities: "Força sobre-humana, Velocidade sobre-humana, Reflexos sobre-humanos, Durabilidade sobre-humana, Fator de cura, Alerta de Sentido de aranha, Sentidos aguçados, Escalar paredes.",
       origin: "Nova Iorque, Estados Unidos da América, Terra."
     },
     {
-      id: 1680307200000,
+      id: 3,
       name: "Wolverine",
       powersAndAbilities: "Sentidos Aguçados, Regeneração, Força Sobre-Humana, Durabilidade Sobre-Humana, Velocidade Sobre-Humana, Reflexos Sobre-Humanos.",
       origin: "Alberta, Canadá, Terra."
     },
     {
-      id: 1691011200000,
+      id: 4,
       name: "Ciclope",
       powersAndAbilities: "Rajada Ótica, Percepção Espacial, Resistência a Energia, Resistência a Energia.",
       origin: "Alasca, Estados Unidos da América, Terra."
     },
     {
-      id: 1700000000000,
+      id: 5,
       name: "Thor",
       powersAndAbilities: "Força divina, controle do clima, voo, teletransporte (com o Mjolnir).",
       origin: "Asgard."
     },
     {
-      id: 1710000000000,
+      id: 6,
       name: "Surfista Prateado",
       powersAndAbilities: "Poder Cósmico (Viagens pelo Cosmos, Força e Durabilidade Extremas, Explosões de Energia).",
       origin: "Alberta, Canadá, Terra."
