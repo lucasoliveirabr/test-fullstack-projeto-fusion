75fcab22a0be4ceeccd22c554f27308cc7655421
lucasoliveirabr <lucas.oliveirabr@outlook.com>
2024-10-29 13:44:10 +0000

    feat: add string normalization in IsContainedIn validator

--- backend/src/heroes/validators/isContainedIn.validator.ts
+++ backend/src/heroes/validators/isContainedIn.validator.ts
@@ -50,6 +50,18 @@ const allowedHeroes = [
   'Serpente da Lua',
 ];
 
+const normalizeString = (str: string): string => {
+  return str
+    .toLowerCase()
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '');
+};
+
+const nameIsContainedInAllowedHeroes = (value: string): boolean => {
+  const normalizedValue = normalizeString(value);
+  return allowedHeroes.some(item => normalizedValue.includes(normalizeString(item)));
+};
+
 export function IsContainedIn(validationOptions?: ValidationOptions) {
   return function (object: Object, propertyName: string) {
     registerDecorator({
@@ -59,7 +71,7 @@ export function IsContainedIn(validationOptions?: ValidationOptions) {
       options: validationOptions,
       validator: {
         validate(value: string) {
-          return allowedHeroes.some(item => value.includes(item));
+          return nameIsContainedInAllowedHeroes(value);
         },
         defaultMessage() {
           return "O nome precisa incluir uma das seguintes opções: Homem-Aranha, Homem de Ferro, Capitão América, Thor, Hulk, Viúva Negra, Gavião Arqueiro, Pantera Negra, Doutor Estranho, Feiticeira Escarlate, Visão, Falcão, Soldado Invernal, Senhor das Estrelas, Groot, Shang-Chi, Homem-Formiga, Capitã Marvel, Demolidor, Tempestade, Wolverine, Jean Grey, Ciclope, Noturno, Fera, Professor X, Adam Warlock, Deadpool, Surfista Prateado, Valquíria, Mulher-Hulk, Falcão Noturno, Patriota de Ferro, Máquina de Combate, Ms. Marvel, Dominó, Longshot, Wiccano, Hulkling, América Chávez, Sersi, Gilgamesh, Thena, Phastos, Makkari, Ajak, Serpente da Lua.";
